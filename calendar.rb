1f485923472294de12668a4dda7d1a6740d2d387
Suzuki Masaya <vagrant@example.com>
2019-05-03 18:57:16 +0900

    [feat]コードをClass化

--- calendar.rb
+++ calendar.rb
@@ -1,65 +1,72 @@
-#!/usr/bin/env ruby
-
 require 'date'
 
-WEEKDAY = {
-    Sun: 0,
-    Mon: 1,
-    Tue: 2,
-    Wed: 3,
-    thu: 4,
-    Fri: 5,
-    Sat: 6,
-}
+class Calendar
 
-def get_first_day_of_the_week(this_year, this_month)
-    Date.new(this_year, this_month, 1).strftime('%a')
-end
+    WEEKDAY = [
+        'Sun',
+        'Mon',
+        'Tue',
+        'Wed',
+        'thu',
+        'Fri',
+        'Sat',
+    ]
 
-def get_last_day(this_year, this_month)
-    Date.new(this_year, this_month + 1, 1).prev_day().day()
-end
-
-def print_calendar(calendar, today)
-    puts(sprintf("%s %d", today.strftime('%B'), today.year).center(20))
-
-    puts(WEEKDAY.keys().map{|w| w.to_s.slice(0,2)}.join(' '))
-
-    calendar.each do |w|
-        puts(w.map{|d| "%2s" % d.to_s}.join(' '))
-    end    
-end
-
-def main()
-    today = Date.today()
-
-    # 月初の曜日を取得
-    first_day = get_first_day_of_the_week(today.year(), today.month())
-    # 月末の日付を取得
-    last_date = get_last_day(today.year(), today.month())
-
-    calendar = []
-    day_per_week = []
-    this_month_dates = 1..last_date
-    index = WEEKDAY[first_day.to_sym]
-
-    this_month_dates.each do |d|
-        weeknum = index % WEEKDAY.length
-
-        day_per_week[weeknum] = d
-        # rubyはインクリメントが使えない...ぐぬぬ...
-        index+=1
-
-        if weeknum == 6
-            calendar.push(day_per_week)
-            day_per_week = []
+    def initialize(month=Date.today().mon(), year=Date.today().year())
+        if month.nil? and year.nil?
+            @target_date = Date.today()
+        else
+            @target_date = Date.new(year, month, 1)
         end
     end
-    # day_per_weekが空配列でない場合はcalendar配列にpushする
-    # これは最終日が土曜日以外の場合のみ実行される
-    calendar.push(day_per_week) if day_per_week
 
-    print_calendar(calendar, today)
+    def display()
+        # 月初のdateオブジェクトを取得
+        first_date = get_first_date()
+        # 月末のdateオブジェクトを取得
+        last_date = get_last_date()
+
+        calendar = []
+        day_per_week = []
+        this_month_days = 1..last_date.day()
+        index = first_date.wday
+
+        this_month_days.each do |d|
+            weeknum = index % WEEKDAY.length
+
+            day_per_week[weeknum] = d
+            # rubyはインクリメントが使えない...ぐぬぬ...
+            index+=1
+
+            if weeknum == 6
+                calendar.push(day_per_week)
+                day_per_week = []
+            end
+        end
+        # day_per_weekが空配列でない場合はcalendar配列にpushする
+        # これは最終日が土曜日以外の場合のみ実行される
+        calendar.push(day_per_week) if day_per_week
+
+        print_calendar(calendar)
+    end
+
+    private
+
+    def get_first_date()
+        Date.new(@target_date.year, @target_date.month, 1)
+    end
+
+    def get_last_date()
+        Date.new(@target_date.year, @target_date.month, -1)
+    end
+
+    def print_calendar(calendar)
+        puts(sprintf("%s %d", @target_date.strftime('%B'), @target_date.year).center(20))
+
+        puts(WEEKDAY.map{|w| w.slice(0,2)}.join(' '))
+
+        calendar.each do |w|
+            puts(w.map{|d| "%2s" % d.to_s}.join(' '))
+        end
+    end
 end
-
-main
